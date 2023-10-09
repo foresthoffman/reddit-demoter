c054c739a5ee8e894b86e38b4b40b611f4ca31c0
Forest Hoffman <forestjhoffman@gmail.com>
2023-10-09 16:18:37 -0500

    Listen for changes to the DOM

--- reddit-demoter.js
+++ reddit-demoter.js
@@ -1,1 +1,12 @@
-document.querySelectorAll(".promotedlink").forEach((el) => el.remove());
+let scrollerParent = document.querySelectorAll("[data-scroller-first]")[0].parentElement;
+scrollerParent.addEventListener("DOMNodeInserted", (e) => {
+    if (e.target.tagName != "SPAN") {
+        return;
+    }
+    let closestPostContainer = e.target.closest('[data-testid="post-container"]');
+    if (closestPostContainer.classList.contains("promotedlink") || closestPostContainer.classList.contains("promotedLink")) {
+        // console.log("AHHA!", closestPostContainer.parentElement.parentElement.parentElement);
+        closestPostContainer.parentElement.parentElement.parentElement.remove();
+        return;
+    }
+});
