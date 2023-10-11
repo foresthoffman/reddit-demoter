660636ed59c0cb24ad8ce2c0342690d69b5a8333
Forest Hoffman <forestjhoffman@gmail.com>
2023-10-11 14:30:38 -0500

    Build 1.3: use MutationObserver

--- reddit-demoter.js
+++ reddit-demoter.js
@@ -1,12 +1,24 @@
-let scrollerParent = document.querySelectorAll("[data-scroller-first]")[0].parentElement;
-scrollerParent.addEventListener("DOMNodeInserted", (e) => {
-    if (e.target.tagName != "SPAN") {
-        return;
-    }
-    let closestPostContainer = e.target.closest('[data-testid="post-container"]');
-    if (closestPostContainer.classList.contains("promotedlink") || closestPostContainer.classList.contains("promotedLink")) {
-        // console.log("AHHA!", closestPostContainer.parentElement.parentElement.parentElement);
-        closestPostContainer.parentElement.parentElement.parentElement.remove();
-        return;
-    }
+// Target the infinite scroll area.
+let focusableDiv = document.querySelector("#SHORTCUT_FOCUSABLE_DIV");
+
+// Wait for promoted posts to be loaded and delete them.
+let observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (mutation.type !== "childList") {
+            return
+        }
+        let promotedPost = mutation.target.closest('[data-testid="post-container"]');
+        if (promotedPost === null || !promotedPost.classList.contains("promotedlink")) {
+            return
+        }
+
+        let promotedPostContainer = promotedPost.parentElement.parentElement.parentElement;
+        promotedPostContainer.remove();
+    });
+});
+
+observer.observe(focusableDiv, {
+    childList: true,
+    characterData: true,
+    subtree : true,
 });
